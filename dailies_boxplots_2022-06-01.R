6c76511b6fe43dd7faa5fa9375b4a9bd47faabe6
Sara <bauer328@umn.edu>
2022-06-01 13:01:25 -0500

    Created new script for dailies summary

---
+++ dailies_boxplots_2022-06-01.R
@@ -0,0 +1,32 @@
+#"Dailies_boxplots_2022-06-01"
+
+#Cleaner code to summarize dailies and to PLOT them! Uses "ALL.csv", which contains the entire dataset.
+#Columns in ALL.csv:
+
+#X (integer)
+#date.time (YYYY-MM-DD HH:MM:SS)
+#unit (C for Celsius)
+#value (temperature in C)
+#name (ex: C2A_R0_air_i106_2020)
+#site
+#rep
+#position
+#buttonID
+#season (YYYY, the year it was downloaded.)
+
+#By: Sara DeLaurentis
+
+
+library(lubridate)
+library(stringr)
+library(tidyverse)
+library(cowplot)
+library(stringr)
+library(dplyr)
+library(glue) #from dplyr
+
+
+setwd("C:/Users/sbaue/coding/R_TEMPRY/Itasca_project_19-21")
+
+data <- read.csv("ALL.csv")
+head(data)
