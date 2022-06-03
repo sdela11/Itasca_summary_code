485a60b0d6b7fd04e39486563b42fb1d44f5893c
Sara <bauer328@umn.edu>
2022-06-03 14:02:40 -0500

    Changed names ("temps") in data loading and df creation process.

--- dailies_boxplots_2022-06-01.R
+++ dailies_boxplots_2022-06-01.R
@@ -30,16 +30,17 @@ library(RColorBrewer)
 setwd("C:/Users/sbaue/coding/R_TEMPRY/Itasca_project_19-21")
 #setwd("C:/Users/sbaue/coding/Itasca_project_19-21")
 
-temps <- read.csv("ALL.csv")
-head(temps)
+temps1 <- read.csv("ALL.csv")
+head(temps1)
 
-temps <- temps %>% group_by(name, date(as.POSIXct(date.time))) %>% 
+
+temps2 <- temps1 %>% group_by(name, date(as.POSIXct(date.time))) %>% 
   mutate(date = date(as.POSIXct(date.time)), 
             meantemp = mean(value), 
             max = max(value), 
             min = min(value)) %>% 
   ungroup()
-temps.s <- temps %>% 
+temps.s <- temps2 %>% 
   distinct(date,name, .keep_all = TRUE)
 
 temps.s[ ,c("X", "unit", "value", "date.time", "date(as.POSIXct(date.time))")] <- NULL #remove those excess columns!
@@ -48,16 +49,17 @@ temps.s[,"treatment"] <- substring(temps.s$site, 1,2)
 head(temps.s)
 str(temps.s)
 
-May.m10 <- temps.s %>%  
+May <- temps.s %>%  
   filter(date >= as.POSIXct("2020-05-01") & date <= as.POSIXct("2020-05-31")) %>% 
-  filter(position == "m10")
+  #filter(position == "m10") %>% 
+  filter(max < 70)
 
-head(May.m10)
-str(May.m10)
+head(May)
+str(May)
 
 #Beginning of ggplot function
 
-data <- temps.s #set data
+data <- May #set data
 f <- ggplot(data, aes(site, max, fill = treatment))
 myplot <- f + geom_boxplot() +
   facet_wrap(vars(position)) +
