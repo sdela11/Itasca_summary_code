71a1b494f4e2b32598e45a84cdcf5f4cb0317e96
Sara <bauer328@umn.edu>
2022-06-15 16:44:44 -0500

    Ran ANOVA and summary for m0surf. With comments.

--- degree_days_anova_2022-06-11.R
+++ degree_days_anova_2022-06-11.R
@@ -63,10 +63,25 @@ fm.lsurf.site <- lm(degree.days ~ site, data = data1[grep("lsurf", data1$positio
 anova(fm.lsurf.site)
 summary(fm.lsurf.site)
 
-#Intercept is C2A (4950), highly significant, D2A is highly significant (.00171), D2B is very highly significant (0.00834)
+#Intercept is C2A (4950), highly significant, D2A is significant (.00106), D2B is very highly significant (0.0352)
+#Adjusted R-squared: 0.6585, p-value: 0.003993.
 
+fm.m0surf.trt <- lm(degree.days ~ treatment, data = data1[grep("m0surf", data1$position),])
+anova(fm.m0surf.trt)
+summary(fm.m0surf.trt)
 
+#significant treatment effect (p = 0.0003256)
+#Intercept is C2, very highly significant (4360), C5 is very highly significant, as is D5. C5 and D5 have around 500 days more than C2.
+#Adj R-squared: 0.5957, p = 0.0003256
 
+fm.m0surf.site <- lm(degree.days ~ site, data = data1[grep("m0surf", data1$position),])
+anova(fm.m0surf.site)
+summary(fm.m0surf.site)
+
+#Site has highly significant effect: p = 2.348e-05.
+#C2A (<2e-16) est: 4451, C5A (0.000198) very highly sig. (above intercept est.)
+#C5B (0.009824) and D5A (0.002762) are highly sig. (above intercept est.)
+#D2A (0.0502) is almost sig. (below intercept est.)
 
 #Attempt at developing function. Not in use at the moment.
 
